a82ae2dfb3049980a0e16de2125541901fb96aa1
Pesaru_Srinivas <Pesaru.srinivas@cubastion.com>
2025-03-10 17:54:03 +0530

    Update react-pipeline.groovy

--- jenkin-pipelines/react-pipeline.groovy
+++ jenkin-pipelines/react-pipeline.groovy
@@ -17,7 +17,7 @@ pipeline {
         }
         stage('Test') {
             steps {
-               //sh 'npm start'
+               sh 'npm start'
                echo "Tested successfully"
             }
         }
